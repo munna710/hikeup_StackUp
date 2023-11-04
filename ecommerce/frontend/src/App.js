3e4226ea512b902d49fea590579b5c25c6e09375
MUNNA <munna7777777.1.0@gmail.com>
2023-11-04 18:13:23 +0530

    start

---
+++ ecommerce/frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {BrowserRouter,Routes,route } from "react-router-dom"
+function App() {
+  return (<>
+    <BrowserRouter>
+    <Routes></Routes>
+    </BrowserRouter>
+    <h1>munna</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ ecommerce/frontend/src/components/layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+};
+export default layout;
+
+
