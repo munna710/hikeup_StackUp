041fe7fba12402c4e0e3c6accd96e0053efdb56d
MUNNA <munna7777777.1.0@gmail.com>
2023-11-05 11:47:29 +0530

    start

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Pdgirls from './pages/Pdgirls';
 function App() {
   return (<>
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path ="About" element={<About />} />
           <Route path ="Contact" element={<Contact />} />
+          <Route path ="Pdgirls" element={<Pdgirls />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ ecommerce/frontend/src/pages/Pdgirls.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from '../components/Header'
+const Pdgirls = () => {
+  return (
+    <Header />
+  )
+}
+
+export default Pdgirls
