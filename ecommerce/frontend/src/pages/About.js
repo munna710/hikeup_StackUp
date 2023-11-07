c51961b5612312a69fb0c02d25aeb39c17d6a973
Fidha07 <120751629+Fidha07@users.noreply.github.com>
2023-11-07 19:30:05 +0530

    update about us

---
+++ ecommerce/frontend/src/pages/About.js
@@ -0,0 +1,94 @@
+//about us page
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className={styles.aboutUs}>
+      <h1 className={styles.aboutUs1}>ABOUT US</h1>
+      <section className={styles.welcomeToCutContainer}>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>
+          Welcome to Cut and Needle – Your Ultimate Fashion Destination for
+          Women and Girls!
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>
+          At Cut and Needle, we believe that fashion is not just about clothing;
+          it's a statement, an expression of your unique style, personality, and
+          confidence. Established with a passion for empowering women and girls
+          through trendy and affordable fashion, Cut and Needle is your go-to
+          online store for all things chic, elegant, and comfortable.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Our Story</p>
+        <p className={styles.blankLine}>
+          Cut and Needle was founded by a group of fashion enthusiasts who
+          shared a common dream: to create a space where women and girls could
+          explore the latest fashion trends, find high-quality clothing, and
+          embrace their individuality. With years of experience in the fashion
+          industry, we set out on a mission to curate a collection that
+          resonates with the diverse tastes and preferences of our customers.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>What Sets Us Apart?</p>
+        <p className={styles.blankLine}>Trendsetting Fashion:</p>
+        <p className={styles.blankLine}>
+          Our team of dedicated fashion experts stays ahead of the curve,
+          keeping an eye on the latest runway trends and styles. We handpick
+          each item in our collection, ensuring that you have access to the most
+          fashionable pieces that the industry has to offer.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Quality You Can Trust:</p>
+        <p className={styles.blankLine}>
+          We understand the importance of quality when it comes to fashion.
+          That's why we collaborate with trusted suppliers and designers to
+          bring you clothing that not only looks good but also feels great to
+          wear. Every piece is carefully inspected to meet our high standards
+          before it reaches your wardrobe.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Affordability and Accessibility:</p>
+        <p className={styles.blankLine}>
+          Fashion should be accessible to everyone. At Cut and Needle, we strive
+          to keep our prices competitive without compromising on quality. We
+          believe that you shouldn't have to break the bank to look and feel
+          fabulous.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Customer-Centric Approach:</p>
+        <p className={styles.blankLine}>
+          Your satisfaction is our top priority. We are committed to providing
+          excellent customer service, ensuring a seamless shopping experience
+          from browsing our website to receiving your order. Have a question or
+          need assistance? Our friendly and knowledgeable support team is always
+          here to help.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Join Us in the Fashion Journey</p>
+        <p className={styles.blankLine}>
+          Whether you're a trendsetter, a fashion enthusiast, or someone looking
+          for the perfect outfit for a special occasion, Cut and Needle invites
+          you to join us on this exciting fashion journey. Explore our wide
+          range of clothing, from casual wear to statement pieces, and discover
+          a world of style waiting for you.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>
+          Thank you for choosing Cut and Needle as your fashion destination. We
+          look forward to being a part of your style story.
+        </p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Happy Shopping!</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>Warm regards,</p>
+        <p className={styles.blankLine}>&nbsp;</p>
+        <p className={styles.blankLine}>The Cut and Needle Team</p>
+      </section>
+      <img className={styles.aboutUsChild} alt="" src="/line-4.svg" />
+    </div>
+  );
+};
+
+
+export default About;
