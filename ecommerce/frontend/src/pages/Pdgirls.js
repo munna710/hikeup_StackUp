f066a6979e4e7434ee5dcb9b9bb817098df1942f
MUNNA <munna7777777.1.0@gmail.com>
2023-11-07 23:10:43 +0530

    updated product catalogue

--- ecommerce/frontend/src/pages/Pdgirls.js
+++ ecommerce/frontend/src/pages/Pdgirls.js
@@ -1,26 +1,10 @@
 //girls product page
-import React from 'react';
-import Header from '../components/Header';
-
+import React from 'react'
+import Header from '../components/Header'
 const Pdgirls = () => {
-  const products = [
-    { id: 1, name: 'Product 1', image: 'image1.jpg', price: 100, description: 'This is product 1' },
-    { id: 2, name: 'Product 2', image: 'image2.jpg', price: 200, description: 'This is product 2' },
-    // Add more products as needed
-  ];
-
   return (
-    <div>
-      {products.map(product => (
-        <div key={product.id}>
-          <img src={product.image} alt={product.name} />
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>${product.price}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    <div>pd</div>
+  )
+}
 
-export default Pdgirls;
+export default Pdgirls
