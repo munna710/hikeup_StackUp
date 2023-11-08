5c2d3d3a4a94de088d221b9f29bfb53b0eb9bbc2
MUNNA <munna7777777.1.0@gmail.com>
2023-11-08 13:06:41 +0530

    updated productCatalogue

--- ecommerce/frontend/src/pages/Pdgirls.js
+++ ecommerce/frontend/src/pages/Pdgirls.js
@@ -1,9 +1,38 @@
 //girls product page
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import { FaHeart } from 'react-icons/fa';
 const Pdgirls = () => {
+  const [heartColor, setHeartColor] = useState('black');
+  
   return (
-    <div>pd</div>
+    <div className='girls-product-catalogue'>
+      <div class="text-wrapper">
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    <h1>GIRLS&nbsp;&nbsp;GIRLS&nbsp;&nbsp;</h1>
+    </div>
+    <div><section className='bg-pd'>
+      <div className="parent-div">
+    <NavLink to="/some-path">
+    <img className='g55' src="\images\g55.png" alt="description" />
+    </NavLink>
+    <FaHeart
+  className='wishlist-icon1'
+  style={{ color: heartColor,stroke: 'black' }}
+  onClick={() => setHeartColor(heartColor === 'black' ? 'red' : 'black')}
+/>
+    <h3 className='g55h3'>Blue net frock</h3>
+    <p className='g55p'>₹600</p>
+    </div>
+    
+  </section></div>
+
+    </div>
   )
 }
 
