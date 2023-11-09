93bd69f1801ee2ac38cf42ab8fd17e9b5d18da2a
MUNNA <munna7777777.1.0@gmail.com>
2023-11-09 11:12:29 +0530

    updated about.js

--- ecommerce/frontend/src/pages/About.js
+++ ecommerce/frontend/src/pages/About.js
@@ -3,7 +3,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <h1>About Us</h1>
+      <h2>Discover Your Style at Cut and Needle</h2>
+      <p>Welcome to Cut and Needle, your one-stop destination for women and girls' fashion. At Cut and Needle, we're passionate about empowering individuals through trendy and affordable clothing.</p>
+      <h2>Why Choose Cut and Needle?</h2>
+      <ul>
+        <li>Trendy Selection: Explore the latest fashion trends curated just for you.</li>
+        <li>Quality Assurance: Enjoy high-quality, stylish clothing designed for comfort and confidence.</li>
+        <li>Affordable Fashion: Access chic and elegant pieces without breaking the bank.</li>
+        <li>Exceptional Service: Our dedicated team ensures a seamless shopping experience and excellent customer service.</li>
+      </ul>
+      <p>Join us at Cut and Needle, where fashion meets affordability. Let your style shine!</p>
+      <p>Happy Shopping!</p>
+      <p>Warm regards,</p>
+      <p>The Cut and Needle Team</p>
+    </div>
   )
 }
 
